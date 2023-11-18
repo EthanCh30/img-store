839fa1706c0d094d0ac32e9eeaeefa6cc4763eae
zhiyun <565306120@qq.com>
2023-11-18 10:48:12 +1030

    gqwe

--- src/main/java/com/devtiro/quickstart/aweawdaw.java
+++ src/main/java/com/devtiro/quickstart/aweawdaw.java
@@ -3,5 +3,6 @@ package com.devtiro.quickstart;
 public class aweawdaw {
     public static void main(String[] args) {
         System.out.println("wqeuihiqwuhe");
+        System.out.println("wqe");
     }
 }
